d3a5ce8abfac4f514fca7039a25abd4049806641
mrpojam <jafari.pouya79@outlook.com>
2023-01-11 19:39:36 +0330

    implememnted views

--- courier/views.py
+++ courier/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Courier, Income
+from .serializer import CourierSerializer, IncomeSerializer
 
 # Create your views here.
+class CourierView(viewsets.ModelViewSet):
+    queryset = Courier.objects.all()
+    seriazer_class = CourierSerializer
+
+class IncomeView(viewsets.ModelViewSet):
+    queryset = Income.objects.all()
+    serializer_class = IncomeSerializer
