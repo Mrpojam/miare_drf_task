f686d74d062d207c49e5bece2c842119b3553c7f
mrpojam <jafari.pouya79@outlook.com>
2023-01-11 15:45:38 +0330

    created courier & trip models

--- courier/models.py
+++ courier/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Courier(models.Model):
+    id = models.PositiveIntegerField(primary_key=True)
+    first_name = models.CharField(max_length=160)
+    last_name = models.CharField(max_length=160)
+    
+    def __str__ (self):
+        pass
+class Trip(models.Model):
+    Date = models.DateTimeField()
+    income = models.BigIntegerField()
+    courier = models.ForeignKey(Courier, on_delete=models.CASCADE)
+    def __str__ (self):
+        pass
+
+    class Meta:
+        ordering = ['Data', 'courier']
